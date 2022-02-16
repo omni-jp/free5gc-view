27e822a67b917ff76a5972d59a1ca79e68a45fe5
HIRONO Koji <koji.hirono.yh@apresiasystems.co.jp>
2022-02-16 14:17:35 +0900

    Add backend

---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.17
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/ghodss/yaml"
+)
+
+const base = "./config"
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if path.Dir(r.URL.Path) != "/configs" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	name := path.Base(r.URL.Path)
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	file := path.Join(base, name)
+	b, err := os.ReadFile(file)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var data map[string]interface{}
+	err = yaml.Unmarshal(b, &data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	text, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(text)
+}
+
+func main() {
+	file := os.Args[1]
+	dir := http.Dir(file)
+	http.Handle("/", http.FileServer(dir))
+	http.HandleFunc("/configs/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			GetHandler(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+	http.ListenAndServe(":3000", nil)
+}
